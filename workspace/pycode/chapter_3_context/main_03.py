14d752ce697a54ec45750d0741b5f9558f635e60
Alexander Humpert <64847974+MirrorWorldCitizen@users.noreply.github.com>
2024-01-20 17:38:00 +0100

    added main_03 file

---
+++ workspace/pycode/chapter_3_context/main_03.py
@@ -0,0 +1,24 @@
+from langchain_community.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+loader = GitHubIssuesLoader(
+    repo = "AlexHumpert/langchain_masterclass",
+    creator = "AlexHumpert",
+    state = "all"
+)
+
+splitter = CharacterTextSplitter(
+    separator = "\n",
+    chunk_size = 200,
+    chunk_overlap = 0
+)
+
+docs = loader.load()
+
+print(docs[0].page_content)
+
+
